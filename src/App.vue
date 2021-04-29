0fa0fd0e659d9b1c2321016dbb99565f23fa6807
NightCoder <laravelmastermmcoder@gmail.com>
2021-04-30 01:34:11 +0630

    16-LifeCycle

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,36 @@
   <h1>Composition API</h1>
   <Calculator />
   <Todo />
+  {{ name }}
 </template>
 
 <script>
+import {
+  onBeforeMount,
+  onBeforeUnmount,
+  onBeforeUpdate,
+  onMounted,
+  onUnmounted,
+  onUpdated,
+  ref,
+} from "vue";
 import Calculator from "./components/Calculator.vue";
 import Todo from "./components/Todo/Todo.vue";
 export default {
   components: { Calculator, Todo },
   name: "App",
-  setup() {},
+  setup() {
+    const name = ref("Aung Myat Oo");
+    setTimeout(() => (name.value = "Someone"), 1000);
+    console.log("Before Create & Created");
+    onBeforeMount(() => console.log("onBeforeMount"));
+    onMounted(() => console.log("onMounted"));
+    onBeforeUpdate(() => console.log("onBeforeUpdate"));
+    onUpdated(() => console.log("onUpdated"));
+    onBeforeUnmount(() => console.log("onBeforeUnmount"));
+    onUnmounted(() => console.log("onUnmounted"));
+    return { name };
+  },
   data() {
     return {
       expense: [

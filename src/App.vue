4d1be8a08b2fef8024d17d3f22463146ea6ced0a
NightCoder <laravelmastermmcoder@gmail.com>
2021-04-30 02:03:50 +0630

    18-ProjectSetup

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <the-hader></the-hader>
+
+  <div class="container">
+    <calculate-transaction></calculate-transaction>
+
+    <transacitons-list></transacitons-list>
+
+    <add-transaciton></add-transaciton>
+  </div>
+</template>
+
+<script>
+import AddTransaciton from "./components/AddTransaciton.vue";
+import CalculateTransaction from "./components/CalculateTransaction.vue";
+import TheHader from "./components/Layout/TheHader.vue";
+import TransacitonsList from "./components/TransacitonsList.vue";
+export default {
+  components: {
+    TheHader,
+    CalculateTransaction,
+    TransacitonsList,
+    AddTransaciton,
+  },
+  name: "App",
+};
+</script>

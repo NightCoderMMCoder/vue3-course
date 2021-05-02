11471abfb38e7410fa7ca069d592b0137b1a122c
NightCoder <laravelmastermmcoder@gmail.com>
2021-05-02 23:43:20 +0630

    43-GlobalRouterGuard

--- src/router/index.js
+++ src/router/index.js
@@ -4,48 +4,73 @@ import Login from "../pages/Auth/Login.vue";
 import Register from "../pages/Auth/Register.vue";
 import { firebaseAuth } from "../firebase/init";
 
+// const requiresAuth = (to, _1, next) => {
+//   if (!firebaseAuth.currentUser) {
+//     next({ name: "Login", query: { redirect: to.fullPath } });
+//   } else {
+//     next();
+//   }
+// };
+
+// const notRequiresAuth = (to, _1, next) => {
+//   if (firebaseAuth.currentUser) {
+//     next({ name: "ChatScreen", query: { redirect: to.fullPath } });
+//   } else {
+//     next();
+//   }
+// };
+
 const routes = [
   {
     path: "/",
     name: "ChatScreen",
     component: ChatScreen,
-    beforeEnter: (_, _1, next) => {
-      if (!firebaseAuth.currentUser) {
-        next({ name: "Login" });
-      } else {
-        next();
-      }
+    meta: {
+      requiresAuth: true,
     },
+    // beforeEnter: requiresAuth,
   },
   {
     path: "/login",
     name: "Login",
     component: Login,
-    beforeEnter: (_, _1, next) => {
-      if (firebaseAuth.currentUser) {
-        next({ name: "ChatScreen" });
-      } else {
-        next();
-      }
+    meta: {
+      requiresGuest: true,
     },
+    // beforeEnter: notRequiresAuth,
   },
   {
     path: "/register",
     name: "Register",
     component: Register,
-    beforeEnter: (_, _1, next) => {
-      if (firebaseAuth.currentUser) {
-        next({ name: "ChatScreen" });
-      } else {
-        next();
-      }
+    meta: {
+      requiresGuest: true,
     },
+    // beforeEnter: notRequiresAuth,
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    if (!firebaseAuth.currentUser) {
+      next({ name: "Login", query: { redirect: to.fullPath } });
+    } else {
+      next();
+    }
+  } else if (to.matched.some((record) => record.meta.requiresGuest)) {
+    if (firebaseAuth.currentUser) {
+      next({ name: "ChatScreen", query: { redirect: to.fullPath } });
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
 });
 
 export default router;

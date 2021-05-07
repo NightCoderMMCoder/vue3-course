b78960852bd63201d0d86eca31f50b57184e0e24
NightCoder <laravelmastermmcoder@gmail.com>
2021-05-08 01:16:34 +0630

    77-GamesList

---
+++ src/components/GamesList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="games-list">
+    <game-item v-for="game in games" :key="game.id" :game="game"></game-item>
+  </div>
+</template>
+
+<script>
+import { computed } from "vue";
+import { useStore } from "vuex";
+import GameItem from "./GameItem.vue";
+
+export default {
+  components: { GameItem },
+  setup() {
+    const store = useStore();
+    const games = computed(() => store.getters.games);
+    return { games };
+  },
+};
+</script>
+
+<style scoped>
+.games-list {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 1200px;
+  margin: 0 auto;
+  margin-top: 40px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,14 @@
 <template>
   <search-box v-model="search"></search-box>
+  <games-list></games-list>
 </template>
 
 <script>
 import { ref } from "vue";
 import SearchBox from "../components/SearchBox.vue";
+import GamesList from "../components/GamesList.vue";
 export default {
-  components: { SearchBox },
+  components: { SearchBox, GamesList },
   setup() {
     const search = ref("");
     return { search };

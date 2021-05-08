c2d39886059fdab5ec0ad9f49eb1036a4d0b6c8d
NightCoder <laravelmastermmcoder@gmail.com>
2021-05-09 01:54:34 +0630

    90-LocalStorage

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,18 @@
 import { createStore } from "vuex";
+import VuexPersistence from "vuex-persist";
+
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage,
+  key: "Todo",
+  reducer: (state) => ({ todos: state.todos }),
+});
+
 let id = 1;
 
 export default createStore({
   state: {
     todos: [],
+    error: "Something went wrong",
   },
   mutations: {
     addTodo(state, text) {
@@ -11,18 +20,17 @@ export default createStore({
         id,
         text,
       });
-      console.log(state.todos);
       id++;
     },
     deleteTodo(state, payload) {
       state.todos = state.todos.filter((todo) => todo.id !== payload);
     },
   },
-  actions: {},
   getters: {
     todos(state) {
       return state.todos;
     },
   },
   modules: {},
+  plugins: [vuexLocal.plugin],
 });
